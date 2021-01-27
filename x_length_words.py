a713b152a13d12a8b64a5d4078b7b72cb952c383
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-27 11:00:15 +0100

    Update x_length_words.py

--- x_length_words.py
+++ x_length_words.py
@@ -1,4 +1,8 @@
+# Python 3 (Coded in 2020)
+
 # Function x_length_words takes a string named _sentence_ and an integer named _x_ as parameters. 
+# First it splits the sentence to get each word as a separate entity, in a list named splited_sentence, 
+# and then compares each word's length with x. If it is less than x it will return False.
 # It returns _True_ if every word in sentence has a length greater than or equal to _x_.
 
 def x_length_words(sentence, x):
